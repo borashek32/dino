5472c2add59e91cce22d6ba46e6734883368d70c
borashek <borashek@inbox.ru>
2022-12-17 16:57:36 +0300

    not ready

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 const dino =  document.getElementById("dino");
 const cactus = document.getElementById("cactus");
+const startButton = document.getElementById("startButton");
+
+
+startButton.addEventListener("click", () => {
+  if (cactus.classList.contains("game__cactusMov")) {
+    cactus.classList.remove("game__cactusMov");
+    startButton.textContent = "start";
+  } else {
+    cactus.classList.add("game__cactusMov");
+    startButton.textContent = "stop";
+  }
+});
+
+document.addEventListener('keyup', function(event){
+  console.log('Key: ', event.key);
+  console.log('keyCode: ', event.keyCode);
+});
 
 document.addEventListener("keydown", (e) => {
-  jump();
+  if (e.keyCode === 38) {
+    jump();
+  }
 });
 
 const jump = () => {
-  if (dino.classList != "jump") {
+  if (dino.classList !== "jump") {
     dino.classList.add("jump");
   }
   setTimeout(function() {
@@ -14,4 +33,10 @@ const jump = () => {
   }, 300);
 };
 
-const isAlive = setInterval()
+const isAlive = setInterval(function() {
+  const dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+  const cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+    alert("GAME OVER");
+  }
+}, 10);
