ed723bca1d22cce96b10233c3ba066a84e94fd8f
borashek <borashek@inbox.ru>
2022-12-17 21:47:08 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -3,13 +3,32 @@ const cactus = document.getElementById("cactus");
 const startButton = document.getElementById("startButton");
 const background = document.getElementById("background");
 const again = document.getElementById("again");
-const later = document.getElementById("later");
-const notificationText = document.getElementById("notification1Text");
 const notification = document.getElementById("notification");
+const one = document.getElementById("oneS");
+const two = document.getElementById("twoS");
+const three = document.getElementById("threeS");
+const time = document.getElementById("time");
 
 
 startButton.addEventListener("click", () => {
+  background.classList.remove("none");
+  one.classList.remove("none");
+  time.classList.remove("none");
+  setTimeout(function () {
+    one.classList.add("none");
+    two.classList.remove("none");
+  }, 1000);
+  setTimeout(function () {
+    two.classList.add("none");
+    three.classList.remove("none");
+  }, 2000);
+  setTimeout(function () {
+    time.classList.add("none");
+    time.classList.remove("time");
+  }, 3000);
   setTimeout(function() {
+    three.classList.add("none");
+    background.classList.add("none");
     if (cactus.classList.contains("cactusMov")) {
       cactus.classList.remove("cactusMov");
       startButton.textContent = "start";
@@ -17,7 +36,7 @@ startButton.addEventListener("click", () => {
       cactus.classList.add("cactusMov");
       startButton.textContent = "stop";
     }
-  })
+  }, 3000);
 });
 document.addEventListener("keydown", (e) => {
   if (e.keyCode === 38) {
@@ -35,27 +54,23 @@ const jump = () => {
     dino.classList.remove("jump");
   }, 300);
 };
-const isAlive = setInterval(function() {
+setInterval(function () {
   const dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
   const cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
   if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
     cactus.classList.remove("cactusMov");
     startButton.textContent = "start";
     background.classList.remove("none");
+    notification.classList.remove("none");
   }
 }, 10);
 again.addEventListener("click", () => {
-  background.classList.add("none");
-});
-later.addEventListener("click", () => {
-  again.classList.add("disappear");
-  later.classList.add("disappear");
+  background.classList.add("disappear");
+  notification.classList.add("disappear");
   setTimeout(function () {
-    again.classList.add("none");
-    later.classList.add("none");
-  }, 1000);
-  setTimeout(function () {
-    notification.classList.add("disappear");
+    background.classList.remove("disappear");
+    notification.classList.remove("disappear");
     background.classList.add("none");
-  }, 2000);
+    notification.classList.add("none");
+  }, 1000);
 });
